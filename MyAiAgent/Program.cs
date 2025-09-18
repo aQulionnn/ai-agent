76f5e708d433d2a12d474b36fd093b98b0e1113d
bekasker <2004bek.asker@mail.ru>
2025-09-18 23:06:58 +0500

    feat: implement GetRoadmap prompt
    - Configure SemanticKernel with Azure OpenAI
    - Add GetRoadmap prompt for JavaPlugins

--- MyAiAgent/Program.cs
+++ MyAiAgent/Program.cs
@@ -1,3 +1,36 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using DotNetEnv;
+using Microsoft.SemanticKernel;
+using Microsoft.SemanticKernel.ChatCompletion;
+using Microsoft.SemanticKernel.Plugins.Core;
 
-Console.WriteLine("Hello, World!");
+Env.Load(Path.Combine(AppContext.BaseDirectory, @"..\..\..\.env"));
+    
+var builder = Kernel.CreateBuilder()
+    .AddAzureOpenAIChatCompletion(
+        Environment.GetEnvironmentVariable("AZURE_OPENAI_DEPLOYMENT_NAME") 
+            ?? throw new InvalidOperationException("AZURE_OPENAI_DEPLOYMENT_NAME environment variable is required"),
+
+        Environment.GetEnvironmentVariable("AZURE_OPENAI_ENDPOINT")
+            ?? throw new InvalidOperationException("AZURE_OPENAI_ENDPOINT environment variable is required"),
+
+        Environment.GetEnvironmentVariable("AZURE_OPENAI_API_KEY")
+            ?? throw new InvalidOperationException("AZURE_OPENAI_API_KEY environment variable is required")
+    );
+
+var kernel = builder.Build();
+
+kernel.ImportPluginFromType<ConversationSummaryPlugin>();
+
+var prompts = kernel.ImportPluginFromPromptDirectory("Prompts/JavaPlugins");
+
+ChatHistory history = [];
+
+string input = @"Я хочу научиться программировать на Java";
+
+var response = await kernel.InvokeAsync<string>(prompts["GetRoadmap"], new() {{ "input", input }});
+
+Console.WriteLine(response);
+
+history.AddUserMessage(input);
+history.AddAssistantMessage(response ?? "");
+
